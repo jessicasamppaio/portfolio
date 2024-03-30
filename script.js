6f6803eeb30f2a75ad6c70e163ba542b1f094a1e
Jessica Sampaio <jessicaspf@gmail.com>
2024-03-30 16:47:39 -0300

    Finalizando com o footer a versão desktop

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', () => {
+    let navLinks = document.querySelectorAll('header nav a');
+    let sections = document.querySelectorAll('section');
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').slice(1) === currentSection) {
+            link.classList.add('active');
+        }
+    });
+});
